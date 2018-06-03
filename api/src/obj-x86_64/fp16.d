29036b81fb0c95a37e3d375e7d2214dab5ca2417
Eason <eason.pan@intel.com>
2018-06-03 13:55:53 -0400

    rm obj

--- api/src/obj-x86_64/fp16.d
+++
@@ -1,3 +0,0 @@
-obj-x86_64/fp16.o: fp16.c ../include/fp16.h
-
-../include/fp16.h:
--- api/src/obj-x86_64/home/eason/workshop/movidius/ncsdk-2.04.00.06/api/src/common/components/XLink/pc/usb_boot.d
+++
@@ -1,8 +0,0 @@
-obj-x86_64//home/eason/workshop/movidius/ncsdk-2.04.00.06/api/src/common/components/XLink/pc/usb_boot.o: \
- /home/eason/workshop/movidius/ncsdk-2.04.00.06/api/src/common/components/XLink/pc/usb_boot.c \
- /usr/include/libusb-1.0/libusb.h \
- /home/eason/workshop/movidius/ncsdk-2.04.00.06/api/src/common/components/XLink/pc/usb_boot.h
-
-/usr/include/libusb-1.0/libusb.h:
-
-/home/eason/workshop/movidius/ncsdk-2.04.00.06/api/src/common/components/XLink/pc/usb_boot.h:
